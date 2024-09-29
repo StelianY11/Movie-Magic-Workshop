c97754ebff2ac8150811b021eba5d16e623aba13
StelianY11 <134134591+StelianY11@users.noreply.github.com>
2024-09-29 14:45:36 +0300

    Creating movies

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,7 +1,16 @@
 import movieData from "../data/movieData.js";
 
-const getAll = () => movieData.getMovies();
+import uniqid from "uniqid";
+
+const getAll = () => movieData.getAll();
+
+const create = (movie) => {
+    movie.id = uniqid();
+
+    return movieData.create(movie);
+};
 
 export default {
     getAll,
+    create,
 }
