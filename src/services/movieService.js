ea3688c458cbbffb2935ad5927e814c430045f5f
StelianY11 <134134591+StelianY11@users.noreply.github.com>
2024-10-01 13:59:08 +0300

    Refactor data layer to mongoose models

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,9 +1,7 @@
-import movieData from "../data/movieData.js";
-
-import uniqid from "uniqid";
+import Movie from "../models/movie.js";
 
 const getAll = async (filter = {}) => {
-    let movies = await movieData.getAll();
+    let movies = await Movie.find();
 
     if(filter.search) {
         movies = movies.filter(m => m.title.toLowerCase().includes(filter.search.toLowerCase()));
@@ -20,20 +18,10 @@ const getAll = async (filter = {}) => {
     return movies;
 };
 
-const create = (movie) => {
-    movie.id = uniqid();
-    movie.rating = Number(movie.rating);
+const create = (movie) =>  Movie.create(movie);
 
-    return movieData.create(movie);
-};
 
-const getOne = async (movieId) => {
-    const movies = await movieData.getAll();
-
-    const resultMovie = movies.find(m => m.id === movieId);
-    
-    return resultMovie;
-};
+const getOne = async (movieId) => Movie.findById(movieId);
  
 export default {
     getAll,
