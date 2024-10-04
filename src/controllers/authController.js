18c8efbdd15914ca7a879546a7e6bb6187ca0df9
StelianY11 <134134591+StelianY11@users.noreply.github.com>
2024-10-04 16:23:00 +0300

    Add basic user creation in DB

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,19 @@
 import { Router } from "express";
 
+import authService from "../services/authService.js";
+
 const router = Router();
 
 router.get('/register', (req, res) => {
     res.render('auth/register');
+});
+
+router.post('/register', (req, res) => {
+    const {email, password, rePassword} = req.body;
+
+    authService.register(email, password);
+
+    res.redirect('/auth/login');
 });
 
 router.get('/login', (req, res) => {
